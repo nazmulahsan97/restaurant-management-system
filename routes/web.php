d716855c6ee33c2ccd68c4e8e98ad9e80ecc834a
BachelorsHome <nazmulahsan97@gmail.com>
2023-11-21 12:19:24 +0600

    sprint-2

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +19,14 @@ use App\Http\Controllers\HomeController;
 
 
 Route::get("/",[HomeController::class,"index"]);
+
+Route::get("/users",[AdminController::class,"user"]);
+
+Route::get("/deleteuser/{id}",[AdminController::class,"deleteuser"]);
+
+
+
+
 
 Route::get("/redirects",[HomeController::class,"redirects"]);
 
